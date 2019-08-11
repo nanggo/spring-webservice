cbe6ec6532f80c32c74503c6f6a4cb0666138f58
nanggo <yamsiri@gmail.com>
2019-08-12 00:25:35 +0900

    Add processing for post request

--- src/main/java/com/nangggo/springwebservice/web/WebRestController.java
+++ src/main/java/com/nangggo/springwebservice/web/WebRestController.java
@@ -1,12 +1,26 @@
 package com.nangggo.springwebservice.web;
 
+import com.nangggo.springwebservice.domain.posts.PostsRepository;
+import com.nangggo.springwebservice.dto.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
+
     @GetMapping("/hello")
     public String hello() {
         return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postsRepository.save(dto.toEntity());
     }
 }
