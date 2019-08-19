f82bff4af6c78e97074ba9c2af37250a887e152d
nanggo <yamsiri@gmail.com>
2019-08-20 01:39:12 +0900

    Implement post service

---
+++ src/main/java/com/nangggo/springwebservice/service/PostsService.java
@@ -0,0 +1,18 @@
+package com.nangggo.springwebservice.service;
+
+import com.nangggo.springwebservice.domain.posts.PostsRepository;
+import com.nangggo.springwebservice.dto.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@AllArgsConstructor
+@Service
+public class PostsService {
+    private PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto dto){
+        return postsRepository.save(dto.toEntity()).getId();
+    }
+}
--- src/main/java/com/nangggo/springwebservice/web/WebRestController.java
+++ src/main/java/com/nangggo/springwebservice/web/WebRestController.java
@@ -2,6 +2,7 @@ package com.nangggo.springwebservice.web;
 
 import com.nangggo.springwebservice.domain.posts.PostsRepository;
 import com.nangggo.springwebservice.dto.posts.PostsSaveRequestDto;
+import com.nangggo.springwebservice.service.PostsService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class WebRestController {
 
-    private PostsRepository postsRepository;
+//    private PostsRepository postsRepository;
+    private PostsService postsService;
 
     @GetMapping("/hello")
     public String hello() {
@@ -20,7 +22,7 @@ public class WebRestController {
     }
 
     @PostMapping("/posts")
-    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
-        postsRepository.save(dto.toEntity());
+    public Long savePosts(@RequestBody PostsSaveRequestDto dto) {
+        return postsService.save(dto);
     }
 }
